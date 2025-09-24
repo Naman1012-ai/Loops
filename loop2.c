2be035c631e130b3cd12e61e3f1f35732d9e8239
Naman <naman25csu156@ncuindia.edu>
2025-09-24 20:23:11 +0530

    Add files via upload

---
+++ loop2.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+//  FIBONACCI SERIES
+
+    // int a = 0, b = 1, c = 0;
+    // int num;
+    // printf("Enter a number : ");
+    // scanf("%d",&num);
+
+
+
+    // while (a <= num) {
+    //     printf("%d ", a);
+    //     c = a + b;
+    //     a = b;
+    //     b = c;
+    // }
+
+    // SECONDS INTO HOURS AND MINUTES
+    // int s,m,h;
+
+    // printf("Enter seconds : ");
+    // scanf("%d",&s);
+
+    // m=s/60;
+    // h=m/60;
+
+    // printf("%dhr:%dmin:%dsec",h,m,s);
+
+
+//    TO PREDICT UPPERCASE, LOWERCASE, OR DIGIT
+    // char ch;
+
+    // printf("Enter a character: ");
+    // scanf("%c", &ch);
+
+    // if (ch >= 'A' && ch <= 'Z') {
+    //     printf("It is an Uppercase letter.\n");
+    // }
+    // else if (ch >= 'a' && ch <= 'z') {
+    //     printf("It is a Lowercase letter.\n");
+    // }
+    // else if (ch >= '0' && ch <= '9') {
+    //     printf("It is a Digit.\n");
+    // }
+    // else {
+    //     printf("It is a Special character.\n");
+    // }
+
+//   int a; 
+//   printf("Enter a number : ");
+//   scanf("%d",&a);
+
+//   (a%2==0) ? printf("Even") : printf("Odd");
+
+
+
+
+
+    
+
+    return 0;
+}
+
+
+
+
+     
